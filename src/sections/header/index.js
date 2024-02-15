ff11978fe150c23283cbec5c3d9add965fd7cf47
Divyanshu Srivastava <dsrivastava65@gmail.com>
2024-02-15 19:12:53 +0530

    header and navbar ui done

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -1,10 +1,49 @@
 import React from "react";
+import style from "./index.module.css";
+import portfolioImage from "../../assets/portfolio_img.jpg";
+
+const socialMediaLinks = [
+  { id: 1, link: "instagram.com", icon: "I" },
+  { id: 1, link: "twitter.com", icon: "T" },
+  { id: 1, link: "dribbble.com", icon: "D" },
+  { id: 1, link: "github.com", icon: "G" },
+];
 
 const Header = () => {
   return (
-    <section id="header">
-      <h2>Header</h2>
-    </section>
+    <header id="header">
+      <div className={`container ${style.header__container}`}>
+        <div className={style.header__profile}>
+          <img src={portfolioImage} alt="Potrait" />
+        </div>
+        <h3>Divyanshu Srivastava</h3>
+        <p>
+          As a Senior Frontend Developer, I bring over a decade of expertise in
+          creating user-centric, responsive designs with HTML5, CSS3, and
+          JavaScript, alongside frameworks like React and Vue.js.
+        </p>
+        <div className={style.header__cta}>
+          <a href="#contact" className="btn primary">
+            Get in touch!
+          </a>
+          <a href="#projects" className="btn light">
+            My projects
+          </a>
+        </div>
+        <div className={style.header__socials}>
+          {socialMediaLinks.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </header>
   );
 };
 
--- src/sections/navbar/index.js
+++ src/sections/navbar/index.js
@@ -1,9 +1,49 @@
 import React from "react";
+import style from "./index.module.css";
+import logo from "../../assets/logo.png";
+import colorPallete from "../../assets/color-palette.png";
+
+const navLinks = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "About",
+    link: "#about",
+  },
+  {
+    id: 3,
+    title: "Projects",
+    link: "#projects",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    link: "#contact",
+  },
+];
 
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Navbar</h2>
+    <nav className="">
+      <div className={`container ${style.nav__container}`}>
+        <a href="">
+          <img src={logo} alt="Logo" className={style.nav__logo} />
+        </a>
+        <ul className={style.nav__menu}>
+          {navLinks.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <span>
+          <img src={colorPallete} className={style.nav__color_palette}></img>
+        </span>
+      </div>
     </nav>
   );
 };
